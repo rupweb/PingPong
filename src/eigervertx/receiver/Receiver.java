bbf8d4327935b2b39447da92a9fa26424f0190d8
Rupert <rupert@webstersystems.co.uk>
2015-09-24 17:30:03 +0100

    UI eventBus testing

--- src/eigervertx/receiver/Receiver.java
+++ src/eigervertx/receiver/Receiver.java
@@ -1,18 +1,35 @@
 package eigervertx.receiver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Handler;
-import io.vertx.core.eventbus.Message;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.json.JsonObject;
 
 public class Receiver extends AbstractVerticle {
+	
+	private static final Logger logger = LogManager.getLogger("Receiver");
+	public EventBus eB;
 
-	  public void start() {
-	    vertx.eventBus().consumer("ui.quotes.request", new Handler<Message<JsonObject>>() {
-	      @Override
-	      public void handle(Message<JsonObject> message) {
-	        System.out.println(message.body().toString());
-	      }
-	    });
-	  }
+  public Receiver(EventBus eB) {
+		this.eB = eB;
 	}
+
+  public void start(Future<Void> startFuture) throws Exception {
+    logger.info("In start");
+      
+	MessageConsumer<JsonObject> quotes = eB.consumer("ui.quotes.response");
+	MessageConsumer<String> data = eB.consumer("ui.marketdata");
+	  
+	quotes.handler(message -> {
+		logger.info("QUOTE: " + message.body().toString());
+	});	
+	
+	data.handler(message -> {
+		logger.info("DATA: " + message.body().toString());
+	});	
+  }
+}
--- src/eigervertx/receiver/VertxApp.java
+++ src/eigervertx/receiver/VertxApp.java
@@ -1,23 +1,34 @@
 package eigervertx.receiver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.EventBus;
 
 public class VertxApp {
+	
+	private static final Logger logger = LogManager.getLogger("VertxApp");	
+	public static EventBus eB;
 
 	public static void main(String[] args) {
 
-		System.out.println("Starting receiver");
+		logger.info("Starting receiver");
 		
 		VertxOptions options = new VertxOptions();
 		
 		Vertx.clusteredVertx(options, res -> {
 		  if (res.succeeded()) {
-		    Vertx vertx = res.result();	    
-			vertx.deployVerticle((Verticle) new Receiver());		    
+		    Vertx vertx = res.result();	
+		    
+		    eB = vertx.eventBus();
+
+		    logger.info("Deploying verticle");
+			vertx.deployVerticle((Verticle) new Receiver(eB));		    
 		  } else {
-		    System.out.println("Failed: " + res.cause());
+			  logger.info("Failed: " + res.cause());
 		  }
 		});
 	}
