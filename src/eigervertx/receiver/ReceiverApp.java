7eac390c3912504adb3a08d1af2c88ac0c2b12f5
Rupert <rupert@webstersystems.co.uk>
2015-11-10 18:27:50 +0000

    TCP v multicast

--- src/eigervertx/receiver/ReceiverApp.java
+++ src/eigervertx/receiver/ReceiverApp.java
@@ -6,10 +6,13 @@ import java.net.UnknownHostException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.hazelcast.config.Config;
+
 import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
 public class ReceiverApp {
 	
@@ -20,10 +23,17 @@ public class ReceiverApp {
 
 		logger.info("Starting receiver");
 		
-		String ipAddress = Inet4Address.getLocalHost().getHostAddress();
-        VertxOptions options = new VertxOptions();
-        options.setClusterHost(ipAddress);
-    	options.setClustered(true);
+		String ipAddress = Inet4Address.getLocalHost().getHostAddress();	
+    	VertxOptions options = new VertxOptions();
+    	Config hazelcastConfig = new Config();
+    	hazelcastConfig.getNetworkConfig().setPortAutoIncrement(false).setReuseAddress(true);
+    	hazelcastConfig.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+    	hazelcastConfig.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
+    	options.setClusterManager(new HazelcastClusterManager(hazelcastConfig));
+    	options.setClusterHost(ipAddress);	
+    	
+    	logger.info("ipAddress: " + ipAddress.toString());
+    	logger.info("options: " + options.toString());
 		
 		Vertx.clusteredVertx(options, res -> {
 		  if (res.succeeded()) {
