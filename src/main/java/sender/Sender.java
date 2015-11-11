6f4971a872a89ce92173d8fd25de6891e06d1bb7
Rupert <rupert@webstersystems.co.uk>
2015-11-11 16:56:09 +0000

    simplify test

--- src/main/java/sender/Sender.java
+++ src/main/java/sender/Sender.java
@@ -21,13 +21,8 @@ public class Sender extends AbstractVerticle {
         logger.info("In Sender");
         
         JsonObject j = new JsonObject();
-              
-        j.put("object", "quoteRequest");
-        j.put("clientRfqId", "TEST12345");
-        j.put("symbol", "EUR/GBP");
-        j.put("quantity", "162500");
-        j.put("settlementDate", "20150928");     
-        j.put("currency", "EUR");
+                  
+        j.put("test", "this");
     	
         logger.info("Sending JSON in 10 seconds: " + j.toString());      
         
