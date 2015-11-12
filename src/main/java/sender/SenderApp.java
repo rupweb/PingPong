9e07576dbe9f298f61e9304dbfbe4ec4278a1d81
Rupert <rupert@webstersystems.co.uk>
2015-11-12 10:12:25 +0000

    ping pong test

--- src/main/java/sender/SenderApp.java
+++ src/main/java/sender/SenderApp.java
@@ -46,7 +46,7 @@ public class SenderApp {
 		
 		VertxOptions options = new VertxOptions();
 		options.setClusterHost(ipAddress);
-		// options.setClustered(true);
+		options.setClustered(true);
     	
     	logger.info("ipAddress: " + ipAddress.toString());
     	logger.info("options: " + options.toString());
